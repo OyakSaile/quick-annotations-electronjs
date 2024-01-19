49544e83b07a984033a52a95ee8f2ea7c46005fb
kayo elias <contatodevkayo@gmail.com>
2024-01-18 23:59:21 -0300

    refactor: remove shadcn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     '@electron-toolkit/eslint-config-prettier'
   ],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
     'no-unused-vars': 'off', // Disable the no-unused-vars rule
     'react/prop-types': 'off' // Disable the prop-types rule for React
   }
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 function App(): JSX.Element {
   return (
-    <div className="container dark h-screen w-full">
-      <h1 className="text-6xl">Hello WOrld.</h1>
+    <div className="min-h-screen dark text-white relative bg-slate-900 p-4 space-y-4 ">
+      <h1 className="text-4xl">Hello World.</h1>
     </div>
   )
 }
